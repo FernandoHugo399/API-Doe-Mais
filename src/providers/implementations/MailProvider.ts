d7d92e963a23f05fcaa24e34a4750f9ef22ca56c
Hugo Fernando <huginho888@outlook.com>
2022-05-10 08:23:12 -0300

    Implemetando SendMail

---
+++ src/providers/implementations/MailProvider.ts
@@ -0,0 +1,33 @@
+import { IMailProvider, IMessage } from '../IMailProvider'
+import nodemailer from 'nodemailer'
+import Mail from 'nodemailer/lib/mailer'
+
+export class MailProvider implements IMailProvider {
+    private transporter: Mail
+
+    constructor () {
+      this.transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT as unknown as number,
+        auth: {
+          user: process.env.USER_HOST,
+          pass: process.env.PASS_HOST
+        }
+      })
+    }
+
+    async sendMail (message: IMessage): Promise<void> {
+      this.transporter.sendMail({
+        to: {
+          name: message.to.name,
+          address: message.to.email
+        },
+        from: {
+          name: message.from.name,
+          address: message.from.email
+        },
+        subject: message.subject,
+        html: message.body
+      })
+    }
+}
